62df24b84d02dec57985370166b2eee95c362561
Saul <smr23.2023m@gmail.com>
2025-11-08 01:18:21 +0100

    feature: landing responsive

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const swiperEl = document.querySelector('.ac-sections swiper-container');
+const swiperRooms = document.querySelector('.ac-rooms-lg swiper-container');
 const nextBtn = document.querySelector('.ac-sections .next-button');
 const prevBtn = document.querySelector('.ac-sections .prev-button');
 
@@ -17,5 +18,31 @@ const swiperParams = {
     }
 };
 
+
+
 Object.assign(swiperEl, swiperParams);
+
 swiperEl.initialize();
+
+
+const changeTab = (tabToOpen) => {
+    const tabHeaders = document.querySelectorAll(".tab-header-element");
+    const serviceTab = document.getElementById("servicesTab");
+    const roomsTab = document.getElementById("roomsTab")
+
+    tabHeaders.forEach((header) => {
+        header.classList.toggle('active')
+    })
+
+    if (tabToOpen == "servicesTab") {
+        roomsTab.style.display = "none";
+        serviceTab.style.display = "block";
+
+    } else {
+        roomsTab.style.display = "block";
+        serviceTab.style.display = "none";
+    }
+
+
+
+}   
